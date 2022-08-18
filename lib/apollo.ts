66cc4f6923438fd4fb43f653968520192c925a16
Mladen <mladen.stankovic90@hotmail.com>
2022-08-18 18:10:45 +0200

    vercel version ApolloClient uri

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 // setup Apollo client
 const client = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: "https://kreativni-univerzum-cicd.vercel.app/api/graphql",
   cache: new InMemoryCache(),
 });
 
